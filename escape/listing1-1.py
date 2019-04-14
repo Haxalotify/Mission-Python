fb077d50fde72f21d4c50afe1f6839a94079d2fc
Alex <harrison.alex117@gmail.com>
2019-04-14 04:22:09 -0700

    added a character and gave it movement

--- escape/listing1-1.py
+++ escape/listing1-1.py
@@ -1,9 +1,25 @@
+
+#window size variables
 WIDTH = 800
 HEIGHT = 600
 player_x = 600
-payer_y = 350
+player_y = 350
 
 def draw():
     screen.blit(images.backdrop, (0, 0))
     screen.blit(images.mars, (50, 50))
+    screen.blit(images.astronaut, (player_x, player_y))
     screen.blit(images.ship, (130, 150))
+
+def game_loop():
+    global player_x, player_y
+    if keyboard.right:
+        player_x += 5
+    elif keyboard.left:
+        player_x -= 5
+    elif keyboard.up:
+        player_x -= 5
+    elif keyboard.down:
+        player_x += 5
+
+clock.schedule_interval(game_loop, 0.03)
