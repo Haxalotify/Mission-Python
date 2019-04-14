4a9662132b45e633dd8b04ef4f83256598522157
Alex <harrison.alex117@gmail.com>
2019-04-14 04:24:47 -0700

    added some comments

--- escape/listing1-1.py
+++ escape/listing1-1.py
@@ -9,17 +9,17 @@ def draw():
     screen.blit(images.backdrop, (0, 0))
     screen.blit(images.mars, (50, 50))
     screen.blit(images.astronaut, (player_x, player_y))
-    screen.blit(images.ship, (130, 150))
+    screen.blit(images.ship, (550, 300))
 
 def game_loop():
-    global player_x, player_y
-    if keyboard.right:
+    global player_x, player_y #adding a player x, player y lets user control right, left, up, and down. if it was
+    if keyboard.right:          #just player x or y, user would only be able to move 2 directions
         player_x += 5
     elif keyboard.left:
         player_x -= 5
     elif keyboard.up:
-        player_x -= 5
+        player_y -= 5
     elif keyboard.down:
-        player_x += 5
+        player_y += 5
 
 clock.schedule_interval(game_loop, 0.03)
